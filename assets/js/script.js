6a698cdc5484a3197f390a65ba91d3ecf7e8b38a
gitlet <11thdim@gmail.com>
2016-07-25 05:22:51 +0400

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// alert('hi');
+$(document).ready(function() {
+  $('.toggle-handle').click(function() {
+    $('.toggle').toggleClass('normal');
+  });
+
+  // sliding to the top
+  // $('.top-slider').click(function() {
+  //   $('')
+  // })
+});
